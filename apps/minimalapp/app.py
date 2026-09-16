2e25286ab6526d73c2f0dcfa537c506e87bcf2f5
Fuka-Oyama <fuka.ooyama@gmail.com>
2026-09-16 09:46:28 +0900

    chore: 問い合わせフォームのテンプレート作成

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -14,7 +14,15 @@ def hello(name):
 def show_name(name):
     return render_template('index.html', name=name)
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('hello-endpoint', name='world'))
-    print(url_for('show_name', name='ichiro', page="1"))
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/contact/complete', methods=['GET', 'POST'])
+def contact_complete():
+    if request.method == 'POST':
+        # メールを送る
+
+        # contactエンドポイントへリダイレクトする
+        return redirect(url_for('contact_complete'))
+    return render_template('contact_complete.html')
